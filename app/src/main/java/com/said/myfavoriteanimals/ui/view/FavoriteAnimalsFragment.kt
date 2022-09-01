26d14a23b8f71d1538e5f8c4218d9e1c2c52435a
Said BİÇER <saidbicer@gmail.com>
2022-09-01 09:30:08 +0300

    RecyclerView ayarları yapıldı

--- app/src/main/java/com/said/myfavoriteanimals/ui/view/FavoriteAnimalsFragment.kt
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/FavoriteAnimalsFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.said.myfavoriteanimals.R
 import com.said.myfavoriteanimals.databinding.FragmentFavoriteAnimalsBinding
+import com.said.myfavoriteanimals.ui.adapter.FavoriteAnimalsAdapter
 import com.said.myfavoriteanimals.ui.viewmodel.AnimalListViewModel
 
 class FavoriteAnimalsFragment : Fragment(R.layout.fragment_favorite_animals) {
 
     private var fragmentBinding: FragmentFavoriteAnimalsBinding? = null
     private lateinit var viewModel: AnimalListViewModel
+    private var animalListAdapter: FavoriteAnimalsAdapter? = null
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,7 +26,13 @@ class FavoriteAnimalsFragment : Fragment(R.layout.fragment_favorite_animals) {
     private fun initialSetups(view: View) {
         fragmentBinding = FragmentFavoriteAnimalsBinding.bind(view)
         viewModel = ViewModelProvider(requireActivity())[AnimalListViewModel::class.java]
+        
+        animalListAdapter = FavoriteAnimalsAdapter(arrayListOf())
 
+        fragmentBinding?.let {
+            it.recyclerView.adapter = animalListAdapter
+            it.recyclerView.layoutManager = LinearLayoutManager(context)
+        }
     }
 
 }
