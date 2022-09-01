b49a9808c8d0c07c4b3c4428012363429467c2cd
Said BİÇER <saidbicer@gmail.com>
2022-09-01 15:21:20 +0300

    Test : AnimalViewModel test edildi

--- app/src/test/java/com/said/myfavoriteanimals/data/repository/FakeAnimalRepository.kt
+++ app/src/test/java/com/said/myfavoriteanimals/data/repository/FakeAnimalRepository.kt
@@ -9,6 +9,7 @@ class FakeAnimalRepository : AnimalRepositoryInterface {
 
     private val animals = mutableListOf<Animal>()
     private val animalsLiveData = MutableLiveData<List<Animal>>(animals)
+    private lateinit var animalModel: AnimalModel
 
     override suspend fun insertAnimalToSQLite(animal: Animal) {
         if (getAnimalWithUrlFromSQLite(animal.imgUrl) == null) {
@@ -22,19 +23,19 @@ class FakeAnimalRepository : AnimalRepositoryInterface {
     }
 
     override suspend fun getAnimalWithUrlFromSQLite(imgUrl: String): Animal? {
-        return animals.filter { it.imgUrl == imgUrl }[0]
+        val list = animals.filter { it.imgUrl == imgUrl }
+        return if (list.isNotEmpty()) list[0] else null
     }
 
     override suspend fun getImageFromAPI(): Resource<AnimalModel> {
-        return Resource.success(
-            AnimalModel(
-                "https://images.dog.ceo/breeds/hound-afghan/n02088094_4517.jpg",
-                "success"
-            )
-        )
+        return Resource.success(animalModel)
     }
 
     private fun refreshData() {
         animalsLiveData.postValue(animals)
+    }
+
+    fun setAnimalModel(message: String, status: String) {
+        animalModel = AnimalModel(message, status)
     }
 }
