5b5cfd03167c5a3fae66c0860db9d72fb45b3849
Said BİÇER <saidbicer@gmail.com>
2022-09-01 07:50:43 +0300

    -Navigasyon sistemi tekmil edildi -Toolbar'a back butonu eklendi

--- app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalFragment.kt
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalFragment.kt
@@ -3,14 +3,41 @@ package com.said.myfavoriteanimals.ui.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.said.myfavoriteanimals.R
+import com.said.myfavoriteanimals.databinding.FragmentAnimalBinding
 
 
 class AnimalFragment : Fragment(R.layout.fragment_animal) {
 
+    private var fragmentBinding: FragmentAnimalBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initialSetups(view)
+
+    }
+
+    private fun initialSetups(view : View) {
+        fragmentBinding = FragmentAnimalBinding.bind(view)
+
+        fragmentBinding?.let { binding ->
+            binding.apply {
+                btnGetRandomImage.setOnClickListener {
+
+                }
+
+                btnSaveImage.setOnClickListener {
+
+                }
+
+                btnListImages.setOnClickListener {
+                    val action = AnimalFragmentDirections.actionAnimalFragmentToAnimalListFragment()
+                    findNavController().navigate(action)
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/said/myfavoriteanimals/ui/view/MainActivity.kt
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/MainActivity.kt
@@ -2,11 +2,29 @@ package com.said.myfavoriteanimals.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.said.myfavoriteanimals.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initialSetups()
     }
+
+    private fun initialSetups() {
+        navHostFragment =  supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.findNavController())
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navHostFragment.findNavController().navigateUp()
+    }
+
 }
