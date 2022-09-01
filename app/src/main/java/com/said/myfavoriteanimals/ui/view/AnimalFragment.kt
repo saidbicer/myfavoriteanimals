1a98279a264c62726424e11ccfe7da3a323cdd89
Said BİÇER <saidbicer@gmail.com>
2022-09-01 07:13:07 +0300

    Gerekli olacak iki fragment oluşturuldu

---
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalFragment.kt
@@ -0,0 +1,16 @@
+package com.said.myfavoriteanimals.ui.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.said.myfavoriteanimals.R
+
+
+class AnimalFragment : Fragment(R.layout.fragment_animal) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalListFragment.kt
@@ -0,0 +1,14 @@
+package com.said.myfavoriteanimals.ui.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.said.myfavoriteanimals.R
+
+class AnimalListFragment : Fragment(R.layout.fragment_animal_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    
+}
