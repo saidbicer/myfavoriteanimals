921d830a246d03f68e16faca95409edfc36c06c1
Said BİÇER <saidbicer@gmail.com>
2022-09-01 08:52:09 +0300

    RoomDb DataBase sınıfı oluşturuldu ve DI modülüne eklendi

--- app/src/main/java/com/said/myfavoriteanimals/di/AppModule.kt
+++ app/src/main/java/com/said/myfavoriteanimals/di/AppModule.kt
@@ -1,12 +1,16 @@
 package com.said.myfavoriteanimals.di
 
+import android.content.Context
+import androidx.room.Room
 import com.said.myfavoriteanimals.data.api.RetrofitAPI
+import com.said.myfavoriteanimals.data.db.MyDatabase
 import com.said.myfavoriteanimals.data.repository.AnimalRepository
 import com.said.myfavoriteanimals.data.repository.AnimalRepositoryInterface
 import com.said.myfavoriteanimals.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,6 +20,12 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun injectRoomDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context, MyDatabase::class.java, "AnimalDB"
+    ).build()
 
     @Singleton
     @Provides
