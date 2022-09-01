eac78d4dba0f329f8c31b3e3897a18f45f9d6650
Said BİÇER <saidbicer@gmail.com>
2022-09-01 08:05:26 +0300

    Retrofit dagger modülüne eklendi

--- app/src/main/java/com/said/myfavoriteanimals/di/AppModule.kt
+++ app/src/main/java/com/said/myfavoriteanimals/di/AppModule.kt
@@ -1,12 +1,28 @@
 package com.said.myfavoriteanimals.di
 
+import com.said.myfavoriteanimals.data.api.RetrofitAPI
+import com.said.myfavoriteanimals.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun injectRetrofitAPI(): RetrofitAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create(RetrofitAPI::class.java)
+    }
+    
 }
