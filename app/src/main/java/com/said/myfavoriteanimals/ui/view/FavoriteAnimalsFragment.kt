7f3ae3d269491f3429db94ea8f9765b24ff29b8e
Said BİÇER <saidbicer@gmail.com>
2022-09-01 09:36:55 +0300

    Db'ten çekilen resim verileri recyclerview üzerinde listelendi

--- app/src/main/java/com/said/myfavoriteanimals/ui/view/FavoriteAnimalsFragment.kt
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/FavoriteAnimalsFragment.kt
@@ -3,6 +3,7 @@ package com.said.myfavoriteanimals.ui.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.said.myfavoriteanimals.R
@@ -16,17 +17,18 @@ class FavoriteAnimalsFragment : Fragment(R.layout.fragment_favorite_animals) {
     private lateinit var viewModel: AnimalListViewModel
     private var animalListAdapter: FavoriteAnimalsAdapter? = null
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initialSetups(view)
+        subscribeObservers()
+        viewModel.getDataFromSQLite()
     }
 
     private fun initialSetups(view: View) {
         fragmentBinding = FragmentFavoriteAnimalsBinding.bind(view)
         viewModel = ViewModelProvider(requireActivity())[AnimalListViewModel::class.java]
-        
+
         animalListAdapter = FavoriteAnimalsAdapter(arrayListOf())
 
         fragmentBinding?.let {
@@ -35,4 +37,20 @@ class FavoriteAnimalsFragment : Fragment(R.layout.fragment_favorite_animals) {
         }
     }
 
+    private fun subscribeObservers() {
+        viewModel.animalsLiveData.observe(viewLifecycleOwner) { list ->
+            if (list.isNotEmpty()) {
+                fragmentBinding?.let {
+                    it.recyclerView.visibility = View.VISIBLE
+                    it.tvEmpty.visibility = View.GONE
+                }
+                animalListAdapter?.updateAnimalList(list)
+            } else {
+                fragmentBinding?.let {
+                    it.recyclerView.visibility = View.GONE
+                    it.tvEmpty.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/said/myfavoriteanimals/ui/viewmodel/AnimalListViewModel.kt
+++ app/src/main/java/com/said/myfavoriteanimals/ui/viewmodel/AnimalListViewModel.kt
@@ -1,10 +1,27 @@
 package com.said.myfavoriteanimals.ui.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.said.myfavoriteanimals.data.db.entity.Animal
+import com.said.myfavoriteanimals.data.model.AnimalModel
+import com.said.myfavoriteanimals.data.repository.AnimalRepositoryInterface
+import com.said.myfavoriteanimals.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AnimalListViewModel @Inject constructor(application: Application) : BaseViewModel(application) {
+class AnimalListViewModel @Inject constructor(private val repository: AnimalRepositoryInterface, application: Application) : BaseViewModel(application) {
+
+    private var _animalsLiveData = MutableLiveData<List<Animal>>()
+    val animalsLiveData: LiveData<List<Animal>>
+        get() = _animalsLiveData
+
+    fun getDataFromSQLite() {
+        launch {
+            _animalsLiveData.value = repository.getAnimalsFromSQLite()
+        }
+    }
 
 }
