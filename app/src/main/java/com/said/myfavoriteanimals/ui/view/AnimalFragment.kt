8ef3ec2758fc942693f438d48d23bc3eb5542cd5
Said BİÇER <saidbicer@gmail.com>
2022-09-01 08:13:12 +0300

    ViewModel sınıfları fragmentlere bağlandı

--- app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalFragment.kt
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalFragment.kt
@@ -3,14 +3,17 @@ package com.said.myfavoriteanimals.ui.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.said.myfavoriteanimals.R
 import com.said.myfavoriteanimals.databinding.FragmentAnimalBinding
+import com.said.myfavoriteanimals.ui.viewmodel.AnimalViewModel
 
 
 class AnimalFragment : Fragment(R.layout.fragment_animal) {
 
     private var fragmentBinding: FragmentAnimalBinding? = null
+    private lateinit var viewModel: AnimalViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,6 +23,7 @@ class AnimalFragment : Fragment(R.layout.fragment_animal) {
 
     private fun initialSetups(view : View) {
         fragmentBinding = FragmentAnimalBinding.bind(view)
+        viewModel = ViewModelProvider(requireActivity())[AnimalViewModel::class.java]
 
         fragmentBinding?.let { binding ->
             binding.apply {
--- app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalListFragment.kt
+++ app/src/main/java/com/said/myfavoriteanimals/ui/view/AnimalListFragment.kt
@@ -3,12 +3,26 @@ package com.said.myfavoriteanimals.ui.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.said.myfavoriteanimals.R
+import com.said.myfavoriteanimals.databinding.FragmentAnimalListBinding
+import com.said.myfavoriteanimals.ui.viewmodel.AnimalListViewModel
 
 class AnimalListFragment : Fragment(R.layout.fragment_animal_list) {
 
+    private var fragmentBinding: FragmentAnimalListBinding? = null
+    private lateinit var viewModel: AnimalListViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initialSetups(view)
+    }
+
+    private fun initialSetups(view: View) {
+        fragmentBinding = FragmentAnimalListBinding.bind(view)
+        viewModel = ViewModelProvider(requireActivity())[AnimalListViewModel::class.java]
+
     }
 
 }
