72a041ad4e2247fac5cc088d360e5b6168e66680
Said BİÇER <saidbicer@gmail.com>
2022-09-01 18:57:51 +0300

    AnimalFragment test edildi

--- app/src/androidTest/java/com/said/myfavoriteanimals/ui/view/AnimalFragmentTest.kt
+++ app/src/androidTest/java/com/said/myfavoriteanimals/ui/view/AnimalFragmentTest.kt
@@ -1,11 +1,19 @@
 package com.said.myfavoriteanimals.ui.view
 
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.MediumTest
+import com.said.myfavoriteanimals.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito
+import javax.inject.Inject
 
 
 @MediumTest
@@ -15,17 +23,29 @@ class AnimalFragmentTest {
     @get: Rule
     val hiltRule = HiltAndroidRule(this)
 
-    @get: Rule
+    @Inject
     lateinit var fragmentFactory: AnimalFragmentFactory
 
     @Before
-    fun setup(){
+    fun setup() {
         hiltRule.inject()
     }
 
     @Test
     fun testNavigationFrom() {
+        val navController = Mockito.mock(NavController::class.java)
 
+        launchFragmentInHiltContainer<AnimalFragment>(fragmentFactory = fragmentFactory) {
+            Navigation.setViewNavController(requireView(), navController)
+        }
+
+        Espresso.onView(ViewMatchers.withId(com.said.myfavoriteanimals.R.id.btnListImages)).perform(
+            ViewActions.click()
+        )
+
+        Mockito.verify(navController).navigate(
+            AnimalFragmentDirections.actionAnimalFragmentToAnimalListFragment()
+        )
     }
 
 }
