dec16f823cdc5eedd89df32e12202ed267feb1e1
Said BİÇER <saidbicer@gmail.com>
2022-09-01 17:23:58 +0300

    Dao hilt ile dest edildi

--- app/src/androidTest/java/com/said/myfavoriteanimals/data/db/dao/AnimalDaoHiltTest.kt
+++ app/src/androidTest/java/com/said/myfavoriteanimals/data/db/dao/AnimalDaoHiltTest.kt
@@ -1,45 +1,54 @@
 package com.said.myfavoriteanimals.data.db.dao
 
+
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import com.said.myfavoriteanimals.data.db.MyDatabase
 import com.said.myfavoriteanimals.data.db.entity.Animal
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
 
 
 @SmallTest
 @ExperimentalCoroutinesApi
-class AnimalDaoTest {
+@HiltAndroidTest
+class AnimalDaoHiltTest {
 
     @get : Rule
-    var instantTastExecutorRole = InstantTaskExecutorRule()
+    var instantTaskExecutorRole = InstantTaskExecutorRule()
 
+    @get : Rule
+    var hiltRule = HiltAndroidRule(this)
 
     private lateinit var dao: AnimalDao
-    private lateinit var database: MyDatabase
+
+    @Inject
+    @Named("testDatabase")
+    lateinit var database: MyDatabase
 
     @Before
-    fun setup(){
-        database = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(), MyDatabase::class.java).allowMainThreadQueries().build()
+    fun setup() {
+        hiltRule.inject()
         dao = database.animalDao()
     }
 
     @After
-    fun teardown(){
+    fun teardown() {
         database.close()
     }
 
     @Test
     fun insertAnimalTest() = runBlocking {
-        val animal = Animal(1,"https://images.dog.ceo/breeds/samoyed/n02111889_6955.jpg")
+        val animal = Animal(1, "https://images.dog.ceo/breeds/samoyed/n02111889_6955.jpg")
         dao.insertAnimal(animal)
 
         val list = dao.getAllAnimals()
